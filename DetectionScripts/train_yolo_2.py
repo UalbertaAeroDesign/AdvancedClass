e2189c5e1e6a1f9261d40e7bab4da63a8906061f
Nikolai <notnikolai1@gmail.com>
2026-02-12 16:01:36 -0700

    Add to gitignore

--- DetectionScripts/train_yolo_2.py
+++ DetectionScripts/train_yolo_2.py
@@ -1,8 +1,9 @@
 from ultralytics import YOLO
 
-# Load a model
-model = YOLO("detection_models\yolo26n.pt")  # load a pretrained model (recommended for training)
+if __name__ == "__main__":
+    # Load a model
+    model = YOLO("detection_models\yolo26n.pt")  # load a pretrained model (recommended for training)
 
-# Train the model
-#results = model.train(data="yolo_images\Trimmed_Images\labelme_json\YOLODataset\dataset.yaml", epochs=100, imgsz=640)
-results = model.train(data="coco.yaml", epochs=100, imgsz=640)
+    # Train the model
+    #results = model.train(data="yolo_images\Trimmed_Images\labelme_json\YOLODataset\dataset.yaml", epochs=100, imgsz=640)
+    results = model.train(data="coco.yaml", epochs=100, imgsz=640)
